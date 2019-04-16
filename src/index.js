e80b95d590bd07f0947def0758658db79e6b4292
Shadow <shadowoboo@gmail.com>
2019-04-17 00:46:37 +0800

    prepare for Output Management

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 import _ from "lodash"; //載入 lodash ，用來簡化陣列之類的東西，讓js好用(?)
-import "./style.css"; //引入css檔(需要設定css的相關loader後，才能import .css進來)
-import "./aa.scss";
-import Icon from "./giphy.gif";
-import Png from "./cookie.png";
-import Data from "./data.xml";
+// import "./style.css"; //引入css檔(需要設定css的相關loader後，才能import .css進來)
+// import "./aa.scss";
+// import Icon from "./giphy.gif";
+// import Png from "./cookie.png";
+// import Data from "./data.xml";
 
 function component(){
     let el=document.createElement("div");
     // el.innerText="Hello webpack";
     el.innerText =_.join(['Hello', 'webpack'], ' ');
-    el.classList.add("hello");
+    // el.classList.add("hello");
 
-    //add img
-    var myIcon=new Image();
-    myIcon.src=Icon;
-    myIcon.classList.add("myIcon");
-    el.appendChild(myIcon);
+    // //add img
+    // var myIcon=new Image();
+    // myIcon.src=Icon;
+    // myIcon.classList.add("myIcon");
+    // el.appendChild(myIcon);
 
-    //add png
-    var myPng = new Image();
-    myPng.src = Png;
-    myPng.classList.add("myPng");
-    el.appendChild(myPng);
+    // //add png
+    // var myPng = new Image();
+    // myPng.src = Png;
+    // myPng.classList.add("myPng");
+    // el.appendChild(myPng);
 
-    //xml test
-    console.log(Data);
+    // //xml test
+    // console.log(Data);
     
     return el;
 }
