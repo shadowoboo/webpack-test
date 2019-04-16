28bafa5483c7123197feb381fb8050b90296b10d
Shadow <shadowoboo@gmail.com>
2019-04-16 19:40:48 +0800

    loader CSS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import _ from "lodash";
+import _ from "lodash"; //載入 lodash ，用來簡化陣列之類的東西，讓js好用(?)
+import "./style.css"; //設定css的相關loader後，引入css檔
 
 function component(){
     let el=document.createElement("div");
     // el.innerText="Hello webpack";
     el.innerText =_.join(['Hello', 'webpack'], ' ');
+    el.classList.add("hello");
     return el;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path=require("path");
 
 module.exports={
-    entry:"./src/index.js",
+    entry:"./src/index.js", //進入點
     output:{
-        filename:"main.js",
+        // filename:"main.js",
+        filename:"bundle.js",
         path:path.resolve(__dirname, "dist")
+    },
+    // module是需要安裝的，安裝在 node_module 才能讓自動化工具抓到來源成功引用
+    module:{
+        rules:[
+            //css解析並放進 index.html裡面
+            //找到 .css檔
+            //先使用 css-loader 載入 .css檔
+            //再使用 style-loader 把檔案插入 html檔 的 header區域
+            //注意: loader順序不可任意調動。 越先接觸檔案放越下面。
+            {
+                test:/\.css$/,//正則表達，找出所以css檔
+                use:[
+                    "style-loader",
+                    "css-loader",
+                ]
+            }
+        ]
     }
 }
