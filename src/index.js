b5f28ddb1ec54c202b5b84830471736154eb7ae1
Shadow <shadowoboo@gmail.com>
2019-04-18 20:28:59 +0800

    HMR 並可以即時重繪，吃到更新後的js

--- src/index.js
+++ src/index.js
@@ -38,11 +38,20 @@ function component(){
     return el;
 }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+
+//在 HMR 並即時更新頁面時，需要能隨時 重新渲染。
+//把元件令出來，方便在 module.hot 時，使用重繪
+let elem = component();
+document.body.appendChild(elem);
+
 
 if(module.hot){
     module.hot.accept("./print.js",function(){
         console.log(`Accepting the updated printMe module!!!`);
-        printMe();
+        // printMe();
+        document.body.removeChild(elem); //先移除該元件
+        elem=component();//重新渲染，把 component() 再次令給 elem
+        document.body.appendChild(elem); //塞回去        
     })
 }
